39a78b4ab674efec3486796fbe6d8b75bb63a924
phamtrunganhwork <143469756+phamtrunganhwork@users.noreply.github.com>
2025-02-15 15:40:15 +0700

    Chapter7

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,41 @@
-export default function Page() {
-  return <p>Page</p>;
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "../ui/fonts";
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from "@/app/lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const c = await fetchCardData();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-x1 md:text-2x1`}>
+        DashBoard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={c.totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={c.totalPendingInvoices} type="pending" />
+        <Card
+          title="Total Invoices"
+          value={c.numberOfInvoices}
+          type="invoices"
+        />
+        <Card
+          title="Total Customers"
+          value={c.numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          <Link href="/dashboard"> Go to DashBoard</Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
